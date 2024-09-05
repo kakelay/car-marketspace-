22fab3af497a0a56e4f2d19b01dd9ec38b1eddf4
Kak Elay <Elay.Kak@amkcambodia.com>
2024-09-05 15:43:22 +0700

    updated code for learn some Funtion

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [valueCount, setValue] = useState(0);
+  function MyButton() {
+    return <button 
+    
+    >I'm a button</button>;
+  }
+  const user = {
+    name: 'Elay.Kak',
+    imageUrl: "https://avatars.githubusercontent.com/u/110383694?v=4",
+    imageSize: 290,
+    userId:'112'
+  };
 
   return (
     <>
@@ -12,11 +24,43 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
+
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
+
+      {/* Add your network image here */}
+      <div>
+        <img
+          src="https://avatars.githubusercontent.com/u/110383694?v=4"
+          alt="Network Image"
+          style={{ width: "200px", height: "auto" }}
+        />
+      </div>
+      <div className="card">
+        <button onClick={() => setValue((valueCount) => valueCount + 1)}>
+          Value is : {valueCount}
+        </button>
+        <button onClick={() => setCount((valueCount) => valueCount + 1)}>
+          count is {valueCount}
+        </button>
+        <div>
+          <MyButton />
+        </div>
+      </div>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+          id:user.userId
+        }}
+      />
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +73,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
